7d0f2c24dabbfed714068d1e89678547b2ea5361
siri25174-sketch <siri25174@gmail.com>
2025-09-18 20:37:03 -0400

    added the func to commit

--- test.go
+++ test.go
@@ -6,4 +6,9 @@ func main() {
 	fmt.Println("Hello, World day 4!")
 	fmt.Println("Hello, World for merge conflict!")
 	fmt.Println("Hello, World after merge conflict!")
+
+	func add ()
+	{
+		fmt.Println("Addition function")
+	}
 }
